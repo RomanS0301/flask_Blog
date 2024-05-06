24bf7e582ec2da4f01c9c3f27b7f45aee35ea389
Romans0301 <roman.sidoruk@gmail.com>
2024-05-06 13:33:04 +0300

    работа с базой данных

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    data_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+@app.route('/')
+def index():  # put application's code here
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
